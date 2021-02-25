95af0b492ca1d03762c6efa7c9ee8661970e5387
joepaul <jonathanpaul16@gmail.com>
2021-02-25 13:06:37 +0100

    Moved initialize firebase to rest, made func async

--- src/rest.ts
+++ src/rest.ts
@@ -1,47 +1,95 @@
-var firebase = require("firebase/app");
+import firebase from "firebase/app";
 
 require("firebase/auth");
-require("firebase/firestore");
+require("firebase/database");
+var firebaseConfig = {
+    apiKey: "AIzaSyCCwlDhG2og5UMLeBqNubMjwrMsxHj8HDI",
+    authDomain: "edaf-90.firebaseapp.com",
+    databaseURL: "https://edaf-90-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "edaf-90",
+    storageBucket: "edaf-90.appspot.com",
+    messagingSenderId: "815688758631",
+    appId: "1:815688758631:web:bed1f352320bd8e122abc2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 
-export const base: {rest:{}} = {rest:{}};
+type Credentials = {
+    username: string, 
+    password: string
+}
 
 
-
-base.rest = {
-    register: (credentials: {username: String, password: String}) => {
-        firebase.auth().createUserWithEmailAndPassword(credentials.username, credentials.password)
-            .then((userCredentials) => {
+export const rest: {register(credentials: Credentials): any, 
+                    login(credentials: Credentials): any, 
+                    logout(): any,
+                    isSignedIn(): any, 
+                    getBookings(sport: string): any[], 
+                    getUserBookings(userid: string): any[]} = {
+    register: async (credentials) => {
+        let result = firebase.auth().createUserWithEmailAndPassword(credentials.username, credentials.password)
+            .then((userCredentials: any) => {
                 //User is now logged in
-                console.log("Login successful")
+                let user = userCredentials.user;
+                alert(`User registered: ${user} and logged in`)
+                return user;
+            })
+            .catch((error: any) => { 
+                console.log(error.message);
+                return undefined;
+            })
+        
+        return await result;
+    },
+
+    login: async (credentials) => {
+        let result = firebase.auth().signInWithEmailAndPassword(credentials.username, credentials.password)
+            .then((userCredentials: {user:any}) => {
+                console.log("User signed in")
                 let user = userCredentials.user;
                 return user;
             })
-            .catch(error => {
-                throw error.message;
+            .catch((error: any) => {
+                console.log("User don't exist");
+                return undefined;
             })
+        
+        return await result;
     },
 
-    login: (credentials: {username: String, password: String}) => {
-        firebase.auth().signInUserWithEmailAndPassworD(credentials.username, credentials.password)
-            .then(userCredentials => {
-                console.log("User registered and signed in")
-                let user = userCredentials.user;
-                return user;
-            })
-            .catch(error => {
-                throw error.message;
-            })
-    },
-
-    logout: () => {
-        firebase.auth().signOut().then(() => {
+    logout: async () => {
+        let result = firebase.auth().signOut().then(() => {
             console.log("Logout successful");
             return true;
-          }).catch((error) => {
+          }).catch((error: any) => {
             throw error.message;
           });
-    }
+
+        
+        return await result;
+    }, 
+
+    isSignedIn: () => {
+        return firebase.auth().currentUser;
+    }, 
+
+
+    getBookings: (sport) => {
+        let bookings=  firebase.database().ref("bookings").child(sport).get().then((snapshot: any) => {
+            if(snapshot.exists())
+                console.log(snapshot.val());
+            else 
+                console.log("No data at bookings");
+        });
+        return [];
+    }, 
+
+    getUserBookings: (userid) => {
+        return [];
+    } 
+ 
+
 
 
 }
