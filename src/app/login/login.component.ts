ce8077df3509c04e28279369a3e779ecad406ad0
Filip <fi8057my-s@student.lu.se>
2021-02-26 14:33:52 +0100

    Can read userinfo again

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -22,7 +22,7 @@ export class LoginComponent implements OnInit {
   }
 
   login(): void {
-    console.log("test", this.userName, this.password);
+    console.log(this.userName, this.password);
   }
 
 }
