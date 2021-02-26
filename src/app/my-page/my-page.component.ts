5db025825f5e5d34c5da8873ee3f9de0276d60b6
David Johansson <david.jsson@gmail.com>
2021-02-26 18:08:56 +0100

    Added my-page component

---
+++ src/app/my-page/my-page.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-my-page',
+    templateUrl: 'my-page.component.html',
+    styleUrls: ['my-page.component.css']
+})
+
+export class MyPageComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
