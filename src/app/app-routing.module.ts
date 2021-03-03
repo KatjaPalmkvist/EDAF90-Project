5b327905fe39d5d4308606d2d439e8053b27610f
joepaul <jonathanpaul16@gmail.com>
2021-03-03 16:55:51 +0100

    Accidentally removed 404 route from app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RegisterComponent } from './register/register.component';
 import { MyPageComponent } from './my-page/my-page.component';
 import { BookingConfirmationComponent} from './booking-confirmation/booking-confirmation.component';
 import { AuthGuard } from './shared/auth.guard';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
@@ -13,7 +14,9 @@ const routes: Routes = [
   {path: 'booking', component: BookingTableComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'booking-confirmation/:sport/:time/:day', component: BookingConfirmationComponent},
-  {path: 'mypage', component: MyPageComponent, canActivate: [AuthGuard]}
+  {path: 'mypage', component: MyPageComponent, canActivate: [AuthGuard]},
+  {path: 'page-not-found', component: PageNotFoundComponent}, 
+  {path: '**', redirectTo: '/page-not-found'}
 ];
 
 @NgModule({
