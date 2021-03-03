94c08f6bafbd719c272f16622010e6d3416b0b87
David Johansson <david.jsson@gmail.com>
2021-03-03 11:40:35 +0100

    Card added to my page

--- src/app/my-page/my-page.component.ts
+++ src/app/my-page/my-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { rest, Sport } from 'src/rest'
 
 @Component({
     selector: 'app-my-page',
@@ -9,5 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class MyPageComponent implements OnInit {
     constructor() { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        console.log(rest.getCurrentUser())
+     }
 }
