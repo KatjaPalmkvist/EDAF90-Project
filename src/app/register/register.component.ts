e3f478d8148715972d60a4c9a95b6ecfa2c4c612
Katja Palmkvist <ka1347pa-s@student.lu.se>
2021-03-03 14:59:17 +0100

    Form validation in register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,17 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  userName: String;
-  password: String;
-  repeat: String;
+  //userName: String;
+  //password: String;
+  //repeat: String;
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { 
+    this.createForm();
+  }
+
+  createForm() {
+    this.registerForm = this.fb.group({
+      userName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      repeat: ['', Validators.required]
+    })
+  }
+
+  get f() { return this.registerForm.controls; }
 
   ngOnInit(): void {
   }
 
-  register(): void {
-    console.log(this.userName, this.password, this.repeat);
+  onSubmit(): void {
+    console.log(this.f.userName.value, this.f.email.value, this.f.password.value, this.f.repeat.value);
   }
 
 }
