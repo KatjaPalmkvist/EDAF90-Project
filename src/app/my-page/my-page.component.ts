92eafdbaa7545685b7e80fd6ac6ee5559844e767
joepaul <jonathanpaul16@gmail.com>
2021-03-03 13:42:13 +0100

    Cards added to mypage

--- src/app/my-page/my-page.component.ts
+++ src/app/my-page/my-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, CanActivate, NavigationEnd, Router } from '@angular/router';
 import { rest, Sport } from 'src/rest'
 
 @Component({
@@ -7,10 +8,22 @@ import { rest, Sport } from 'src/rest'
     styleUrls: ['my-page.component.css']
 })
 
-export class MyPageComponent implements OnInit {
-    constructor() { }
-
+export class MyPageComponent implements OnInit, CanActivate {
+    constructor(private router: Router, private route: ActivatedRoute) { 
+        
+    }
     ngOnInit() {
-        console.log(rest.getCurrentUser())
-     }
+
+    }
+
+    canActivate() {
+
+        if(!rest.getCurrentUser().uid) {
+            console.log("We are here baby!")
+            this.router.navigate(["/login"], {relativeTo: this.route});
+            return false;
+        }
+        console.log("Fuck you man")
+        return true;
+    }
 }
