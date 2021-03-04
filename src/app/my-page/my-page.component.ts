efd07a159be54f8793a6d323425b6cf98415eebf
David Johansson <david.jsson@gmail.com>
2021-03-04 09:51:17 +0100

    Added date filter in my page

--- src/app/my-page/my-page.component.ts
+++ src/app/my-page/my-page.component.ts
@@ -8,14 +8,28 @@ import { rest } from 'src/rest'
 })
 
 export class MyPageComponent implements OnInit {
-    userBookings: {[date: string]: {[sport: string]: string[]}};
+    userBookings: {[date: string]: {[sport: string]: string[]}} = {};
+    oldBookings: {[date: string]: {[sport: string]: string[]}} = {};
     
     constructor() { 
 
     }
     ngOnInit() {
         rest.getUserBookings(rest.getCurrentUser().uid).then((res: any) => {
-            this.userBookings = res;
+            const today = new Date();
+            today.setHours(0,0,0,0);
+            const tmp: {[date: string]: {[sport: string]: string[]}} = res;
+            let dates = Object.keys(tmp);
+            dates.forEach(date => {
+                let date_arr = date.split("-").map(x => Number(x));
+                let this_date = new Date(date_arr[0], date_arr[1] - 1, date_arr[2]);
+                if (this_date < today) {
+                    this.oldBookings[date] = tmp[date];
+                } else {
+                    this.userBookings[date] = tmp[date];
+                }
+            })
+            
 
         })
         
